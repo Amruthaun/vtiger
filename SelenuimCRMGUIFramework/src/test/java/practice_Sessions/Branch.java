0e14fcddf611d09635af50aadba7978b05c1b881
Admin <Admin@DESKTOP-I3EH5DN.mshome.net>
2025-03-10 17:09:51 +0530

    push to global repository

---
+++ SelenuimCRMGUIFramework/src/test/java/practice_Sessions/Branch.java
@@ -0,0 +1,9 @@
+package practice_Sessions;
+
+public class Branch {
+
+	public static void main(String[] args) {
+		System.out.println("repository");
+	}
+
+}
---
+++ SelenuimCRMGUIFramework/src/test/java/practice_Sessions/PushAndPull.java
@@ -0,0 +1,16 @@
+package practice_Sessions;
+
+public class PushAndPull {
+
+	public static void main(String[] args) {
+		int i=34;
+		int j=56;
+		 i=i+j;
+		 j=i-j;
+		 i=i-j;
+		System.out.println(i+" is the i value");
+		System.out.println(j+" is the j value");
+		
+	}
+
+}
